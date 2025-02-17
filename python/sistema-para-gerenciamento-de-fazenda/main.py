14a78e3a25b46578f3625541c648995713a5c864
lucasarcoverdem <lucasarcoverdem@gmail.com>
2025-02-17 20:59:55 -0300

    Update main.py

--- python/sistema-para-gerenciamento-de-fazenda/main.py
+++ python/sistema-para-gerenciamento-de-fazenda/main.py
@@ -19,7 +19,53 @@ def normalize(text):
 
 def register_bovine():
     print('\n- Registrar bovino')
-    
+
+    name = input('\nInsira o nome do bovino: ').strip().lower().capitalize()
+
+    while True:
+        gender = normalize(input('Insira o gênero do bovino (boi, vaca): ').strip())
+
+        if gender in ['boi', 'vaca']:
+            break
+        else:
+            print('\nEntrada inválida. Por favor, digite "boi" ou "vaca".')    
+
+    breed = input('Insira a raça do bovino: ').strip().lower().capitalize()
+
+    while True:
+        try:
+            age = float(input('Insira a idade do bovino (anos, ex: 0.5 para 5 meses): ').strip())
+            if age < 0:
+                print("\nA idade não pode ser negativa. Tente novamente.")
+            else:
+                break
+        except ValueError:
+            print('\nEntrada inválida. Digite um número válido (ex: 0.5 para 5 meses).')
+
+    while True:    
+        try:
+            weight_kg = float(input('Insira o peso do bovino (kg): ').strip())
+            if weight_kg <= 0:
+                print("\nO peso deve ser maior que zero.")
+            else:
+                break
+        except ValueError:
+            print('\nEntrada inválida. Digite um número válido (ex: 500.5).')
+
+    while True:
+        try:
+            market_value = float(input('Insira o valor de mercado do bovino (R$): ').strip())
+            if market_value < 0:
+                print("\nO valor de mercado não pode ser negativo.")
+            else:
+                break
+        except ValueError:
+            print('\nEntrada inválida. Digite um número válido (ex: 3500.75).')
+
+    new_bovine = Bovine(name, gender, breed, age, weight_kg, market_value)
+    cattle.append(new_bovine)
+
+    print(f'\n{name} adicionado com sucesso!')
 
 
 def cattle_function():
@@ -41,13 +87,13 @@ def cattle_function():
     elif menu == '3':
         pass
     elif menu == '4':
-        main()
+        return 
     else:
         print("\nOpção inválida. Tente novamente.")
 
 
 def login():
-    user = input('Digite seu nome de usuário: ').lower().capitalize()
+    user = input('Digite seu nome de usuário: ').strip().lower().capitalize()
     password = input('Digite sua senha: ')
 
     print('Entrando. . .')
